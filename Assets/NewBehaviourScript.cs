fb1ae5293eeda00f83eb5ee82115dc12d5277f89
Arat-Mektay <70063502+Arat-Mektayev@users.noreply.github.com>
2023-09-25 20:11:47 +0300

    TestCommit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("Achtung!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
